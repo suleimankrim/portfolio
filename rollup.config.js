1ae6d4f2c7cc7712a9617e4799c3011e9bc19c26
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-07-03 00:02:25 +0300

    homeWork

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 
 export default {
   input: 'src/main.ts',
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,61 @@
-'use strict';
-import {format } from 'date-fns'
-
-const b= format(new Date(2023, 1, 11), 'yyyy-MM-dd')
-
-const t = document.getElementById('kk') as HTMLElement;
-t.innerText = b;
-const fetchId =async () => {
-    const params = new URLSearchParams();
-    params.append("email", "s.karimeddin@innopolis.university");
-    console.log("https://fwd.innopolis.university/api/hw2?" + params.toString());
-    const response = await fetch(
-        "https://fwd.innopolis.university/api/hw2?" + params.toString()
-    );
-    const id: string = await response.json();
-    return id;
+import { formatDistanceToNow } from "date-fns";
+const fetchId = async () => {
+  const params = new URLSearchParams();
+  params.append("email", "s.karimeddin@innopolis.university");
+  console.log("https://fwd.innopolis.university/api/hw2?" + params.toString());
+  const response = await fetch(
+    "https://fwd.innopolis.university/api/hw2?" + params.toString()
+  );
+  const id: string = await response.json();
+  return id;
 };
-interface joke{
-    img:string;
-    alt:string;
-    year:string;
-    month:string;
-    safe_title:string;
-    day:string;}
-const fetchComic=async () => {
-    const params = new URLSearchParams();
-    const id = await fetchId();
-    params.append("id",id);
-    console.log("https://fwd.innopolis.university/api/comic?" + params.toString());
-    const response = await fetch("https://fwd.innopolis.university/api/comic?" + params.toString());
-    const data : joke= await response.json();
-    return data;
+interface joke {
+  img: string;
+  alt: string;
+  year: string;
+  month: string;
+  safe_title: string;
+  day: string;
+}
+const fetchComic = async () => {
+  const params = new URLSearchParams();
+  const id = await fetchId();
+  params.append("id", id);
+  console.log(
+    "https://fwd.innopolis.university/api/comic?" + params.toString()
+  );
+  const response = await fetch(
+    "https://fwd.innopolis.university/api/comic?" + params.toString()
+  );
+  const data: joke = await response.json();
+  return data;
 };
-const f= async ()=>{
-const comm = await fetchComic();
-console.log(comm);
-const image = document.getElementById("comicc") as HTMLImageElement;
-image.src = comm.img;
-image.alt =comm.alt
-const title = document.getElementById("title") as HTMLElement;
-title.innerText = comm.safe_title;
-const date = document.getElementById("date") as HTMLElement;
-const dat= new Date(comm.month.toString()+"/"+comm.day.toString()+"/"+comm.year.toString());
-date.innerText = dat.toLocaleDateString();}
+const f = async () => {
+  const comm = await fetchComic();
+  console.log(comm);
+  const image = document.getElementById("comicc") as HTMLImageElement;
+  image.src = comm.img;
+  image.alt = comm.alt;
+  const title = document.getElementById("title") as HTMLElement;
+  title.innerText = comm.safe_title;
+  const date = document.getElementById("date") as HTMLElement;
+  const dat = new Date(
+    comm.month.toString() +
+      "/" +
+      comm.day.toString() +
+      "/" +
+      comm.year.toString()
+  );
+  date.innerText = dat.toLocaleDateString();
+  const b =formatDistanceToNow(new Date(
+    comm.month.toString() +
+      "/" +
+      comm.day.toString() +
+      "/" +
+      comm.year.toString()
+  ));
+  const t = document.getElementById("kk") as HTMLElement;
+  t.innerText = b;
+};
 f();
-console.log("cfwokeeeeeeeeeeeeeeeee")
+console.log("cfwokeeeeeeeeeeeeeeeee");
