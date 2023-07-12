57c5d019760f3542e0ef57a02eab1c55a4c8aea7
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-07-12 16:32:52 +0300

    go

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { page } from '$app/stores';
-	import github from '../../static/img/github.svg';
-	import facebook from '../../static/img/facebook-svgrepo-com.svg';
-	import tweter from '../../static/img/twitter-svgrepo-com.svg';
+	import github from '../../static/github.svg';
+	import facebook from '../../static/img/svg/facebook-svgrepo-com.svg';
+	import tweter from '../../static/img/svg/twitter-svgrepo-com.svg';
 	export const prerender = true;
 </script>
 
